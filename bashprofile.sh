20f0c3f96f084047c6ee7fb6171742d9a0f60b41
cademccu <cade21mccumber@gmail.com>
2024-07-07 16:16:05 -0600

    Update 2024

--- bashprofile.sh
+++ bashprofile.sh
@@ -2,8 +2,6 @@
 #----------------------------------------------------------
 #   GENERAL
 #----------------------------------------------------------
-# delete this file [ ~/.hushlogin ] for last login information
-# FUCK YOU APPLE I DONT WANt ZSH
 export BASH_SILENCE_DEPRECATION_WARNING=1
 export PATH=$PATH:~/scripts					# add scripts directory to path
 
@@ -29,14 +27,12 @@ export PYTHONPATH="/usr/local/anaconda3/latest/lib/python3.8/site-packages:$PYTH
 tail -n 41 ~/scripts/TEXT_FILES/penta.txt | head -n 32
 cat ~/scripts/TEXT_FILES/fatbet.txt
 # delete this file [ ~/.hushlogin ] for last login information
-# FUCK YOU APPLE I DONT WANt ZSH
-export BASH_SILENCE_DEPRECATION_WARNING=1
 
 #----------------------------------------------------------
 #   TERMINAL CHANGES
 #----------------------------------------------------------
 set -o vi							# set terminal line to use vi
-cd() { builtin cd "$@"; ls; }               			# Always list directory contents upon 'cd'
+# cd() { builtin cd "$@"; ls; }               			# Always list directory contents upon 'cd'
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
@@ -62,33 +58,38 @@ alias qsize="du -sh "						# prints size of directory in human readable format.
 alias psize="sips -Z "						# resizes a photo. Args are <PIXEL MIN> <PHOTO_NAME>
 alias h='history | sort -rn | more'				# opens "more" with history in reverse order (most recent)
 alias chist="history | sort -rn | grep 'cd' "			# searches for cd commands
+alias lsone="ls -1"
+alias ls1="ls -1"
 #----------------------------------------------------------
 #   WORKSPACE MODIFIER
 #----------------------------------------------------------
 
-function setaa() { aa=$PWD; printf "\naa is set to: $aa\n\n"; }
-function aa() { cd $aa; }
+function setaa() { aa="$PWD"; echo "aa is set to: $aa"; }
+function aa() { cd $aa; pwd; ls; }
 
-function setbb() { bb=$PWD; printf "\nbb is set to: $bb\n\n"; }
-function bb() { cd $bb; }
+function setbb() { bb="$PWD"; echo "bb is set to: $bb"; }
+function bb() { cd $bb; pwd; ls; }
 
-function setcc() { cc=$PWD; printf "\ncc is set to: $cc\n\n"; }
-function cc() { cd $cc; }
+function setcc() { cc="$PWD"; echo "cc is set to: $cc"; }
+function cc() { cd $cc; pwd; ls; }
 
-function setdd() { dd=$PWD; printf "\ndd is set to: $dd\n\n"; }
-function dd() { cd $dd; }
+function setdd() { dd="$PWD"; echo "dd is set to: $dd"; }
+function dd() { cd $dd; pwd; ls; }
 
-function setee() { ee=$PWD; printf "\nee is set to: $ee\n\n"; }
-function ee() { cd $ee; }
+function setee() { ee="$PWD"; echo "ee is set to: $ee"; }
+function ee() { cd $ee; pwd; ls; }
 
-function setff() { ff=$PWD; printf "\nff is set to: $ff\n\n"; }
-function ff() { cd $ff; }
+function setff() { ff="$PWD"; echo "ff is set to: $ff"; }
+function ff() { cd $ff; pwd; ls; }
 
-function setdata() { MYDATA=$PWD; printf "\nMYDATA is set to: $MYDATA\n\n"; }
-function data() { cd $MYDATA; }
 
-function setwork() { MYWORK=$PWD; printf "\nMYWORK is set to: $MYWORK\n\n"; }
-function work() { cd $MYWORK; }
+function setdata() { MYDATA="$PWD"; echo "MYDATA is set to: $MYDATA"; }
+function data() { cd $MYDATA; pwd; ls; }
+
+function setwork() { MYWORK="$PWD"; echo "MYWORK is set to: $MYWORK"; }
+function work() { cd $MYWORK; pwd; ls; }
+
+
 
 function pwds() {
 	printf "\n=============================================\n"
@@ -121,15 +122,3 @@ function getpath {
 }
 
 
-
-
-#----------------------------------------------------------
-#   UNUSED STUFF
-#----------------------------------------------------------
-# if i ever want to use node.js again which lets be honest, no
-#export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-#export PATH="/usr/local/sbin:$PATH"
-#export PATH="$HOME/.jenv/bin:$PATH"
-#eval "$(jenv init -)"
